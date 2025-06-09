1ea43c50b654e1c6bd2e8d1c8ffc61610d9ec5aa
Aryanpatel-7 <ap0905749@gmail.com>
2025-06-09 13:06:20 +0530

    Initial commit

---
+++ src/Page/Home.jsx
@@ -0,0 +1,53 @@
+import React, { useContext, useState } from 'react'
+import Nav from '../Component/Nav';
+import Categories from '../Category';
+import Card from '../Component/Card';
+import food_items from '../food';
+import { dataContext } from '../context/UserContext';
+
+const Home = () => {
+  let {cate,setcate,input}=useContext(dataContext)
+
+  function filter (Category){
+  if (Category==="All") {
+    setcate(food_items)
+  }else{
+    let newList=food_items.filter((item)=>(item.food_category===Category))
+    setcate(newList)
+  }
+  }
+  return (
+    <div className='bg-slate-200 w-full min-h-screen'>
+       <Nav/> 
+       {!input?<div className='flex flex-wrap justify-center items-center
+         gap-5 w-[100%]'>
+        {Categories.map((item)=>{
+          return <div className='w-[140px] h-[150px] bg-white
+           flex flex-col items-start gap-5 p-5 justify-start
+            text-[20px] font-semibold text-gray-600 rounded-lg shadow-xl hover:bg-green-200 coursor-pointer transition-all
+             duration-200' onClick={()=>filter(item.name)}>
+            {item.icon} 
+            {item.name}
+            
+          </div>
+       
+        })}
+       </div>:null}   
+       
+       <div className='w-full flex flex-wrap gap-5 px-5 justify-center
+         items-center pt-8 pb-8'>
+         {cate.map((item)=>(
+          <Card name={item.food_name} image={item.food_image}
+            price={item.price} id={item.id} type={item.food_type}
+          />
+         ))}
+       </div>
+
+
+    
+       
+    </div>
+  )
+}
+
+export default Home;
